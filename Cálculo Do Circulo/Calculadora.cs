caccc7a56eac8865a2b45e2606a799bd463823f1
Jooper8 <joopereight@gmail.com>
2021-09-08 21:32:48 -0300

    Resolvido erro ao crashar o Visual Studio.

---
+++ Cálculo Do Circulo/Calculadora.cs
@@ -0,0 +1,30 @@
+﻿//Frederico Guilherme Camilli Proenla RA:200925
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cálculo_Do_Circulo
+{
+    //Classe na qual serão realizados os cálculos solicitados pelo programa principal.
+    class Calculadora
+    {
+        //Instanciação da variável Pi, que neste caso sempre será 3.14.
+        public double Pi = 3.14;
+
+        //Instanciação da variável Circunferencia.
+        public double Circunferencia(double r)
+        {
+            //Fórmula para calcular o resultado da variável Circunferencia.
+            return 2.0 * Pi * r;
+        }
+
+        //Instanciação da variável Volume.
+        public double Volume(double r)
+        {
+            //Fórmula para calcular o resultado da variável Volume.
+            return 4.0 / 3.0 * Pi * r * r * r;
+        }
+    }
+}
---
+++ Cálculo Do Circulo/Program.cs
@@ -0,0 +1,32 @@
+﻿//Frederico Guilherme Camilli Proenla RA:200925
+using System;
+using System.Globalization;
+
+namespace Cálculo_Do_Circulo
+{
+    //Programa principal com o qual o usuário interagirá.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instanciado a calculadora "calc" que utilizará as variáveis da classe "Calculadora".
+            Calculadora calc = new Calculadora();
+
+            //Instanciando as variáveis raio, circ e volume.
+            double raio, circ, volume;
+
+            //Pedido do valor do raio ao usuário.
+            Console.WriteLine("Insira o valor do raio:");
+            raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            //Cálculo da circunferência e do volume, utilizando variáveis já instanciadas e as fórmulas Circunferencia e Volume já introduzidas na classe Calculadora.
+            circ = calc.Circunferencia(raio);
+            volume = calc.Volume(raio);
+
+            //Anunciando os resultados ao usuário, convertendo os resultados à string.
+            Console.WriteLine("Circunferência: " + circ.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Volume: " + volume.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Valor de Pi: " + calc.Pi.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
